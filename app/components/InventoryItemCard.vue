6ca0ba88f33f5785349d57382264f6cdcf665668
Venczel Patrik <v.papatrik@gmail.com>
2025-10-28 12:34:35 +0100

    feat: add inventory item mock

--- app/components/InventoryItemCard.vue
+++ app/components/InventoryItemCard.vue
@@ -1,5 +1,22 @@
 <template>
-  <UCard>Todo implement Inventory Item Card Component</UCard>
+  <UCard>
+    <div>
+      <NuxtImg :src="item.image_url" :alt="item.name" class="size-30" />
+      <h2>{{ item.name }}</h2>
+      <p>Mennyiség: {{ item.quantity }}</p>
+      <!-- TODO implement timeAgo funcionnality -->
+      <p>Utoljára frissítve: {{ item.lastUpdated }}</p>
+    </div>
+  </UCard>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+//TODO replace mock with props:
+const item: InventoryItem = {
+  id: "ID-1",
+  name: "Mock item",
+  image_url: "https://dummyimage.com/120x120/1a3138/fff",
+  quantity: 10,
+  lastUpdated: "2025-10-28T15:30:47Z",
+};
+</script>
